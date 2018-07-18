adb6aab264f49c27c32dc9e4d9cd0d15d7faaca3
jennygrahamjones <jenny.grahamjones@gmail.com>
2018-07-18 14:46:26 +0100

    GCI-60 | Update path to schema in validation handler

--- app/utils/SchemaValidationHandler.scala
+++ app/utils/SchemaValidationHandler.scala
@@ -28,7 +28,7 @@ trait SchemaValidationHandler {
     val validator = new SchemaValidator()
 
     val schema: JsValue = {
-      val path = ("resources/public/api/conf/1.0/schemas/real-time-income-information-post-schema.json")
+      val path = ("public/api/conf/1.0/schemas/real-time-income-information-post-schema.json")
         Json.parse(Source.fromFile(path).mkString)
     }
 
