fe6573f264029eeac422abd4cc3161c3c8be2b1d
Derek Gray <derekgray@Dereks-MacBook-Pro.local>
2018-08-22 13:01:32 +0100

    GCI-114 | Upgrade bootstrap-play-25

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.8.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "2.0.0",
     "uk.gov.hmrc" %% "domain" % "5.2.0",
     "com.eclipsesource" %% "play-json-schema-validator" % "0.8.9",
     "uk.gov.hmrc" %% "play-auditing" % "3.4.0"
