d910dc77c66b0f3438d9b8673090dd474d4cdbd5
Dandyboyo2 <45761631+Dandyboyo2@users.noreply.github.com>
2025-12-30 15:02:07 +0000

    DDCNL-11422 | PR bot comment addressed

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcBoostrapVersion       = "10.4.0"
+  private val hmrcBoostrapVersion       = "10.5.0"
   private val hmrcMongoVersion          = "2.11.0"
   private val hmrcDomainVersion         = "13.0.0"
 
